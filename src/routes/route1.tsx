03aea61280e0c88d87f6ab6fdafd518a59d57d02
Omar Salem <omarsalem0721@gmail.com>
2022-10-08 15:07:20 +0200

    Configure routes for the app

---
+++ src/routes/route1.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Route1 = () => {
+  return (
+    <div>route1</div>
+  )
+}
+
+export default Route1
---
+++ src/routes/route2.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Route2 = () => {
+  return <div>route2</div>;
+};
+
+export default Route2;
